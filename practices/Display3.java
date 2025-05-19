3d8d9738c5fc0d97ca81f155a49452988cd2075c
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-05-19 19:32:36 +0300

    add another display program

---
+++ practices/Display3.java
@@ -0,0 +1,17 @@
+public class Display3{
+public static void main(String []args)
+{
+xafsa();   
+}
+static void xafsa(){
+sayid();
+System.out.println("xafsa method la wacay");
+}
+static void sayid(){
+//faysal(); 
+
+System.out.println("sayid method la wacay");
+
+}
+}
+}
