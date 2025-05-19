82f45678a6c2d0b6e5ce59d64311d65e3918dd06
Hafsa Hassan <xafsaxaaji936@gmail.com>
2025-05-19 19:32:17 +0300

    add Display program

---
+++ practices/Display2.java
@@ -0,0 +1,15 @@
+public class Display2{
+public static void main (String args[])
+{
+show();
+}
+static void show(){
+show1();
+System.out.println("show method ayaa lawacay");
+}
+static void show1(){
+//show1();
+System.out.println("sow1 method ayaa lawacay");
+}
+}
+}
